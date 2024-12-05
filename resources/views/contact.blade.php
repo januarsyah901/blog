0cd25419dede972636b6bddb3733dd0a7fc35b5e
januarsyah901 <januarsyahakbar791@gmail.com>
2024-12-05 16:29:03 +0700

    hallo

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact</title>
+</head>
+<body>
+    <h1>Contact Us</h1>
+    <ul>
+        <li>
+            <a href="http://instagram.com/hallojanuu">Instagram</a>
+        </li>
+        <li>
+            <a href="https://x.com/Januarsyah901">X</a>
+        </li>
+        <li>
+            <a href="https://www.linkedin.com/in/januarsyah">Linkedin</a>
+        </li>
+    </ul>
+</body>
+</html>
