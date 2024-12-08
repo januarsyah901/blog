6cb2c6b014968fa13439b716cfb4377d3f010b5d
januarsyah901 <januarsyahakbar791@gmail.com>
2024-12-08 15:09:47 +0700

    masa harus di commit dulu sih

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,25 +5,26 @@
             <h2 class="display-4 mb-3 text-center">Artikel</h2>
             <p class="lead fs-lg mb-10 text-center px-md-16 px-lg-21 px-xl-0">Here are the latest company news from our blog that got the most attention.</p>
             <div class="carousel owl-carousel blog grid-view" data-margin="30" data-dots="true" data-autoplay="false" data-autoplay-timeout="5000" data-responsive='{"0":{"items": "1"}, "768":{"items": "2"}, "992":{"items": "2"}, "1200":{"items": "3"}}'>
+                @foreach($posts as $post)
                 <div class="item">
                     <article>
-                        <figure class="overlay overlay1 hover-scale rounded mb-5"><a href="#"> <img src="./assets/img/photos/b4.jpg" alt="" /></a>
+                        <figure class="overlay overlay1 hover-scale rounded mb-5"><a href="#"> <img src="./assets/img/photos/{{ $post['img'] }}.jpg" alt="" /></a>
                             <figcaption>
                                 <h5 class="from-top mb-0">Read More</h5>
                              </figcaption>
                         </figure>
                         <div class="post-header">
                             <div class="post-category text-line">
-                                <a href="#" class="hover" rel="category">Coding</a>
+                                <a href="#" class="hover" rel="category">{{ $post['category'] }}</a>
                             </div>
                             <!-- /.post-category -->
-                            <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="./blog-post.html">Ligula tristique quis risus</a></h2>
+                            <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="{{ $post['id'] }}">{{ $post['post_title'] }}</a></h2>
                         </div>
                         <!-- /.post-header -->
                         <div class="post-footer">
                             <ul class="post-meta">
-                                <li class="post-date"><i class="uil uil-calendar-alt"></i><span>14 Apr 2021</span></li>
-                                <li class="post-comments"><a href="#"><i class="uil uil-comment"></i>4</a></li>
+                                <li class="post-date"><i class="uil uil-calendar-alt"></i><span>{{ $post['published_at'] }}</span></li>
+                                <li class="post-author"><a href="#"><i class="uil uil-user"></i><span>{{ $post['author'] }}</span></a></li>
                             </ul>
                             <!-- /.post-meta -->
                         </div>
@@ -32,86 +33,7 @@
                     <!-- /article -->
                 </div>
                 <!-- /.item -->
-                <div class="item">
-                    <article>
-                        <figure class="overlay overlay1 hover-scale rounded mb-5"><a href="#"> <img src="./assets/img/photos/b5.jpg" alt="" /></a>
-                            <figcaption>
-                                <h5 class="from-top mb-0">Read More</h5>
-                            </figcaption>
-                        </figure>
-                        <div class="post-header">
-                            <div class="post-category text-line">
-                                <a href="#" class="hover" rel="category">Workspace</a>
-                            </div>
-                            <!-- /.post-category -->
-                            <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="./blog-post.html">Nullam id dolor elit id nibh</a></h2>
-                        </div>
-                        <!-- /.post-header -->
-                        <div class="post-footer">
-                            <ul class="post-meta">
-                                <li class="post-date"><i class="uil uil-calendar-alt"></i><span>29 Mar 2021</span></li>
-                                <li class="post-comments"><a href="#"><i class="uil uil-comment"></i>3</a></li>
-                            </ul>
-                            <!-- /.post-meta -->
-                        </div>
-                        <!-- /.post-footer -->
-                    </article>
-                    <!-- /article -->
-                </div>
-                <!-- /.item -->
-                <div class="item">
-                    <article>
-                        <figure class="overlay overlay1 hover-scale rounded mb-5"><a href="#"> <img src="./assets/img/photos/b6.jpg" alt="" /></a>
-                            <figcaption>
-                                <h5 class="from-top mb-0">Read More</h5>
-                            </figcaption>
-                        </figure>
-                        <div class="post-header">
-                            <div class="post-category text-line">
-                                <a href="#" class="hover" rel="category">Meeting</a>
-                            </div>
-                            <!-- /.post-category -->
-                            <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="./blog-post.html">Ultricies fusce porta elit</a></h2>
-                        </div>
-                        <!-- /.post-header -->
-                        <div class="post-footer">
-                            <ul class="post-meta">
-                                <li class="post-date"><i class="uil uil-calendar-alt"></i><span>26 Feb 2021</span></li>
-                                <li class="post-comments"><a href="#"><i class="uil uil-comment"></i>6</a></li>
-                            </ul>
-                            <!-- /.post-meta -->
-                        </div>
-                        <!-- /.post-footer -->
-                    </article>
-                    <!-- /article -->
-                </div>
-                <!-- /.item -->
-                <div class="item">
-                    <article>
-                        <figure class="overlay overlay1 hover-scale rounded mb-5"><a href="#"> <img src="./assets/img/photos/b7.jpg" alt="" /></a>
-                            <figcaption>
-                                <h5 class="from-top mb-0">Read More</h5>
-                            </figcaption>
-                        </figure>
-                        <div class="post-header">
-                            <div class="post-category text-line">
-                                <a href="#" class="hover" rel="category">Business Tips</a>
-                            </div>
-                            <!-- /.post-category -->
-                            <h2 class="post-title h3 mt-1 mb-3"><a class="link-dark" href="./blog-post.html">Morbi leo risus porta eget</a></h2>
-                        </div>
-                        <div class="post-footer">
-                            <ul class="post-meta">
-                                <li class="post-date"><i class="uil uil-calendar-alt"></i><span>7 Jan 2021</span></li>
-                                <li class="post-comments"><a href="#"><i class="uil uil-comment"></i>2</a></li>
-                            </ul>
-                            <!-- /.post-meta -->
-                        </div>
-                        <!-- /.post-footer -->
-                    </article>
-                    <!-- /article -->
-                </div>
-                <!-- /.item -->
+                @endforeach
             </div>
             <!-- /.owl-carousel -->
         </div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,60 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
+
+// 'Controller'
+class Posts
+{
+    public static function all()
+    {
+        return [
+        [
+            'id' => '1',
+            'post_title' => 'Kamu siapa ini dimana siapa namanya',
+            'slug' => 'kamu-siapa-ini-dimana-siapa-namanya',
+            'img' => 'b4',
+            'category' => 'Web Programming',
+            'published_at' => '5 March 2020',
+            'author' => 'Januarsyah',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12'
+        ],
+        [
+            'id' => '2',
+            'post_title' => 'Kamu siapa ini dimana siapa namanya',
+            'slug' => 'kamu-siapa-ini-dimana-siapa-namanya',
+            'img' => 'b5',
+            'category' => 'Web Programming',
+            'published_at' => '5 March 2020',
+            'author' => 'Januarsyah',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12'
+        ],
+        [
+            'id' => '3',
+            'post_title' => 'Kamu siapa ini dimana siapa namanya',
+            'slug' => 'kamu-siapa-ini-dimana-siapa-namanya',
+            'img' => 'b6',
+            'category' => 'Web Programming',
+            'published_at' => '5 March 2020',
+            'author' => 'Januarsyah',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12'
+        ],
+        [
+            'id' => '4',
+            'post_title' => 'Kamu siapa ini dimana siapa namanya',
+            'slug' => 'kamu-siapa-ini-dimana-siapa-namanya',
+            'img' => 'b7',
+            'category' => 'Web Programming',
+            'published_at' => '5 March 2020',
+            'author' => 'Januarsyah',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12'
+        ],
+    ];
+    }
+}
+# route home
 Route::get('/', function () {
     return view('home',[
         'title' => 'Home'
@@ -9,14 +62,22 @@ Route::get('/', function () {
 });
 
 # route blog
-Route::get('/post', function () {
-    return view('post',[
-        'title' => 'Blog',
-        'author' => 'Januarsyah',
-        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptates. lorem12'
+Route::get('/posts', function () {
+    return view('posts',[
+        'title' => 'Blog', 'posts' => Posts::all()
     ]);
 });
 
+# route single post
+Route::get('/posts/{slug}', function ($slug) {
+    $post = Arr::first(Posts::all(), function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+    return view('post',[
+        'title' => 'Single Post',
+        'post' => $post
+    ]);
+});
 Route::get('/about', function () {
     return view('about',[
         'title' => 'Januarsyah',
@@ -26,3 +87,5 @@ Route::get('/about', function () {
         'phone' => '081234567890'
     ]);
 });
+
+
